3a7064eb1f52fc63fddf8df3eacda5bc69358597
Abena Okyere <60201239+abena00742@users.noreply.github.com>
2020-09-24 02:15:36 +0100

    Update form.js

--- form.js
+++ form.js
@@ -2,27 +2,10 @@ function pop(){
     alert("Thank you! Your form is submitted");
 }
 
-function Email(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-
-    if (Email(email)) {
-        $result.text(email + " is valid :)");
-        $result.css("color", "green");
-      } else {
-        $result.text(email + " is not valid :(");
-        $result.css("color", "red");
-      }
-      return false;
-}
-
-
-function work(){
-    var x = document.getElementById("addForm");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-      text += x.elements[i].value + "<br>";
-    }
-    document.getElementById("demo").innerHTML = text;      
-}
+    document.getElementById("First_name").innerHTML = localStorage.getItem("fnm");
+    document.getElementById("Middle_name").innerHTML = localStorage.getItem("mnm");
+    document.getElementById("Last_name").innerHTML = localStorage.getItem("lnm");
+    document.getElementById("Ema_il").innerHTML = localStorage.getItem("eml");
+    document.getElementById("Pho_ne").innerHTML = localStorage.getItem("phn");
+    document.getElementById("Add_ress").innerHTML = localStorage.getItem("adr");
+    document.getElementById("Gen_der").innerHTML = localStorage.getItem("gen");
